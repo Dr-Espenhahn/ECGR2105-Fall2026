4c6c053d9d815365e817d2db137f773c37412115
Dr-Espenhahn <lespenha@charlotte.edu>
2026-08-17 13:19:57 -0400

    First lecture - Hello World

---
+++ Hello_World/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream> //Imorting cout
+
+using namespace std; //Don't have to include std::
+
+int main() {
+
+    cout << "Hello, World!" << endl; //sending to print in terminal
+
+    return 0; //No error, return 0
+
+}
