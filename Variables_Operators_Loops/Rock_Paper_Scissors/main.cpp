38a57926f10c2cde853b7c6c9e5d287c3fd28e99
Dr-Espenhahn <lespenha@charlotte.edu>
2026-08-28 12:14:47 -0400

    Lecture 6: Matrix & Functions

--- Variables_Operators_Loops/Rock_Paper_Scissors/main.cpp
+++ Variables_Operators_Loops/Rock_Paper_Scissors/main.cpp
@@ -1,48 +1,77 @@
 #include <iostream>
 #include <string>
-#include <cstdlib> //gives us rand() & srand()
-#include <ctime> //gives us time()
+#include <cstdlib> // gives us rand() & srand()
+#include <ctime> // gives us time()
 
 #include <vector>
-#include <iomanip> //gives setprecision()
+#include <iomanip> // gives setprecision()
 
 using namespace std;
+
+const int RPS_THROW_COUNT = 3;
+
+string toLowerString(string upperCaseString){
+    //Make string (char array) lowercase
+    for(int j = 0; j < upperCaseString.length(); j++){
+        upperCaseString.at(j) = tolower(upperCaseString.at(j));
+    }
+
+    return upperCaseString;
+}
+
+int encodeRPS(string userInput, string decodeRPS[RPS_THROW_COUNT]){
+    userInput = toLowerString(userInput);
+
+    for(int i=0; i<RPS_THROW_COUNT; i++){
+        string keyValue = toLowerString(decodeRPS[i]);
+        if(userInput == keyValue)
+            return i;
+    }
+    return -1; // Error
+}
 
 int main(){
     string userInput;
     unsigned int computerInput; 
-    const unsigned int GAME_COUNT = 5; //unsigned = never negative
-    unsigned int compArrayInputs[GAME_COUNT]; //Array size must be defined
-    vector<unsigned int> compInputs; //Dyamic size arrays
+    vector<unsigned int> compInputs; // Dyamic size arrays
     unsigned int gameCount;
+    string decodeRPS[RPS_THROW_COUNT] = {"Rock", "Paper", "Scissors"}; // MUST be lowercase
 
     int gameWinCount = 0;
     double winRate = 0.0;
 
-    srand(time(0)); //Seed our random number generator
+    srand(time(0)); // Seed our random number generator
+
 
     /*
-    //Vector commands
+    // Arrays - defined size
+    //unsigned int compInputs[gameCount]; //DOES NOT WORK, need to define array size at compilation not run
+    const unsigned int GAME_COUNT = 5; //unsigned = never negative
+    unsigned int compArrayInputs[GAME_COUNT]; //Array size must be defined
+    for(int i=0; i < GAME_COUNT; i++){
+        compArrayInputs[0] = rand() % 3;
+    }
+    */
+
+    /*
+    // Vector commands
     compInputs.push_back(1); // .push_back(val) -- Add 'val' to the end of the vector
     cout << compInputs.back() << endl; // .back() -- Get us last variable in the vector
     compInputs.pop_back(); // .pop_back -- Removes the last variable in the vector
     */
 
+    //cout << i <<endl; //Does not work; i only defined within "for" loop
+
+
+    // Vectors - dynamic size
     cout << "How many games would you like to play: ";
     cin >> gameCount;
-
-    for(int i=0; i < GAME_COUNT; i++){
-        compArrayInputs[0] = rand() % 3;
-    }
-
-    //cout << i <<endl; //Does not work; i only defined within "for" loop
 
     for(int i = 0; i < gameCount; i++){
         compInputs.push_back(rand() % 3);
     }
 
-    //unsigned int compInputs[gameCount]; //DOES NOT WORK, need to define array size at compilation not run
-
+    
 
     //while(gameCountdown-- > 0){
     for(int i=0; i < gameCount ; i++){
@@ -51,13 +80,56 @@ int main(){
         cout << "Rock, paper, scissors, Shoot! ";
         cin >> userInput;
 
+        int encodedUserInput = encodeRPS(userInput);
+
+        string resultOutput = "";
+
+        if(encodedUserInput == compInputs.at(i)){
+            resultOutput = "It's a tie! You both threw " + decodeRPS[encodedUserInput];
+        }else{
+            switch(encodedUserInput){
+                case 0:
+                    switch(compInputs.at(i)){
+                        case 1: //Paper
+                            cout << "Computer played " << decodeRPS[compInputs.at(i)] <<  ", you lost." << endl;
+                            break;
+                        case 2: //Scissors
+                            cout << "Computer played scissors, you won!" << endl;
+                            userWin = true;
+                            break;
+                        default:
+                            cout << "Error." << endl;
+                            return 0;
+                    }
+                break;
+                case 1:
+
+                break;
+                case 2:
+
+                break;
+                default:
+                    cout << "Error 1" << endl;
+                break;
+            }
+        }
+
+
+        /*
+        //Moved into function toLowerString()
+
         //Make string (char array) lowercase
         for(int j = 0; j < userInput.length(); j++){
             userInput.at(j) = tolower(userInput.at(j));
         }
+        */
 
-        computerInput = rand() % 3; // 0 = Rock, 1 = Paper, 2 = Scissors
+        //Make string (char array) lowercase
+        //userInput = toLowerString(userInput);
 
+        //computerInput = rand() % 3; // 0 = Rock, 1 = Paper, 2 = Scissors
+
+        
         /*
         int i = 0;
         while(i < userInput.length()){
@@ -80,7 +152,7 @@ int main(){
 
         
             
-        
+        /*
         if(userInput == "rock" ){ //'OR': userInput == "Rock" || userInput == "rock" //Note: 'AND' is &&
             cout << "User input rock." << endl;
             /*
@@ -92,7 +164,7 @@ int main(){
             
             }else{
 
-            }*/ 
+            }*//* 
         
             switch(compInputs.at(i)){
                 case 0: //Rock
@@ -115,13 +187,14 @@ int main(){
             //TODO: Implement
             switch(compInputs.at(i)){
                 case 0: //Rock
+                    cout << "Computer played rock, you won!" << endl;
                     userWin = true;
                     break;
                 case 1: //Paper
-                userWin = true;
+                    cout << "Computer played paper, you tied." << endl;
                     break;
                 case 2: //Scissors
-
+                    cout << "Computer played scissors, you lost." << endl; 
                     break;
                 default:
                     cout << "Error." << endl;
@@ -132,13 +205,14 @@ int main(){
             //TODO: Implement
             switch(compInputs.at(i)){
                 case 0: //Rock
-                    
+                    cout << "Computer played rock, you lost." << endl;
                     break;
                 case 1: //Paper
+                    cout << "Computer played paper, you won!" << endl;
                     userWin = true;
                     break;
                 case 2: //Scissors
-                    
+                    cout << "Computer played scissors, you tied." << endl;
                     break;
                 default:
                     cout << "Error." << endl;
@@ -153,6 +227,7 @@ int main(){
             gameWinCount++;
             cout << "Win count: " << gameWinCount << endl;
         }
+        */
     }
 
     // gameWinCount -- integer (int)
--- Variables_Operators_Loops/introMatrix.cpp
+++ Variables_Operators_Loops/introMatrix.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -6,9 +7,21 @@ int main(){
 
     int matrix[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
 
-    for (int i : matrix[1]){
-        cout << i;
-    } cout << endl;
+    vector<vector<int>> matrix2; //allows various sized 2d vector
+
+    vector<int> vectorName;
+
+    vector<vector<int>> vector2D;
+
+    // matrix[1] == {4,5,6}
+    for (int j=0; j < 3; j++){
+        for (int i : matrix[j]){
+            cout << i;
+        } 
+        cout << endl;
+    }
+    
+
 
     return 0;
 }
