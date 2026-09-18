a362f9551677952812f1dbc3a3f0b79958013ff9
Dr-Espenhahn <lespenha@charlotte.edu>
2026-09-18 15:12:44 -0400

    9/18 lecture

--- Pointers/Classes_Pointers/main.cpp
+++ Pointers/Classes_Pointers/main.cpp
@@ -53,18 +53,95 @@ public:
     }
 };
 
+
+
+
+
+
 class Library{
 private:
     vector<Book*> books;
+    //vector<int> copy_count;
 
 public:
     void addBook(Book* book){
-        books.push_back(book);
+        if(findBook(book->getTitle()) == nullptr){
+            books.push_back(book);
+            //copy_count.push_back(1);
+        }else{
+            cout << "Book is already in the library." << endl;
+            /*
+            for(int i=0; i<books.size(); i++){ //standard "for" loop
+                if(books.at(i)->getTitle() == book->getTitle()){
+                    copy_count.at(i)++; //increment copy count
+                }
+            }
+            */
+        }
     }
+
+    void printLibrary() const {
+        /*
+        for(int i=0; i<books.size(); i++){ //standard "for" loop
+            books.at(i); //do something
+        }*/
+       cout << "Libary Books: " << endl;
+
+        for(Book* book : books){ // "for-each" loop
+            // book; //--> same thing as books.at(i); in previous look
+            book->printInfo();
+        }
+    }
+
+    Book* findBook(string title){
+        for (Book* book : books){
+            if(book->getTitle() == title) {
+                return book;
+            }
+        }
+
+        return nullptr;
+    }
+
 };
 
+
+
+
+
+
+
+
 int main(){
-    vector<Book> storedBooks;
+    //vector<Book> storedBooks;
+    Library library;
+
+    Book book1("The Hobbit", "J.R.R. Tolkein");
+    Book* book2 = new Book("1984", "George Orwell");
+    Book* book3 = new Book("Dune", "Frank Herbert");
+
+    library.addBook(&book1);
+    library.addBook(book2);
+    library.addBook(book3);
+
+    //library.printLibrary();
+
+    Book* foundBook = library.findBook("1984");
+
+    if(foundBook != nullptr){
+        cout << foundBook->getTitle() << " exists.";
+    }else{
+        cout << "Searched book does not exist";
+    }
+
+     Book* foundBook = library.findBook("1984");
+
+    if(foundBook != nullptr){
+        cout << foundBook->getTitle() << " exists.";
+    }else{
+        cout << "Searched book does not exist";
+    }
+    
 
     return 0;
 }
