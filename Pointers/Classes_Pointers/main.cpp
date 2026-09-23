d5062ffd93fdd189ebfc0f36da2f1e26ebba49b1
Dr-Espenhahn <lespenha@charlotte.edu>
2026-09-23 11:59:07 -0400

    checkout (with error)

--- Pointers/Classes_Pointers/main.cpp
+++ Pointers/Classes_Pointers/main.cpp
@@ -51,6 +51,12 @@ public:
             cout << title << " was not checked out." << endl;
         }
     }
+
+    /*
+    ~Book(){
+        cout << "deleting Book" << endl;
+    }
+    */
 };
 
 
@@ -101,13 +107,16 @@ public:
         }
 
         return nullptr;
+    }  
+
+    ~Library(){
+        cout << "deleting Library" << endl;
+        for(Book* book : books){
+            delete book;
+        }
     }
 
 };
-
-
-
-
 
 
 
@@ -116,31 +125,65 @@ int main(){
     //vector<Book> storedBooks;
     Library library;
 
-    Book book1("The Hobbit", "J.R.R. Tolkein");
+    Book* book1 = new Book("The Hobbit", "J.R.R. Tolkein");
     Book* book2 = new Book("1984", "George Orwell");
     Book* book3 = new Book("Dune", "Frank Herbert");
 
-    library.addBook(&book1);
+    library.addBook(book1);
     library.addBook(book2);
     library.addBook(book3);
 
+    string crInput;
+    string findTitle;
+    Book* foundBook = library.findBook(findTitle);
+
     //library.printLibrary();
 
-    Book* foundBook = library.findBook("1984");
+    //Book* foundBook = library.findBook("1984");
 
-    if(foundBook != nullptr){
-        cout << foundBook->getTitle() << " exists.";
-    }else{
-        cout << "Searched book does not exist";
+    library.printLibrary();
+
+    
+    cout << "Do you want to checkout (c) or return (r): ";
+    getline(cin, crInput);
+
+    if(crInput == "c"){
+        // Getting user input of book to checkout
+        cout << "Title of book to checkout: ";
+        getline(cin, findTitle);
+
+        if(foundBook != nullptr){
+            // cout << foundBook->getTitle() << " exists." << endl;
+            foundBook->checkOut();
+        }else{
+            cout << findTitle << " is not in the library." << endl;
+        }
+    } else if (crInput == "r"){
+        library.printLibrary();
+
+        cout << "Title of book to return: ";
+        getline(cin, findTitle);
+        foundBook = library.findBook(findTitle);
+
+        if(foundBook != nullptr){
+            // cout << foundBook->getTitle() << " exists." << endl;
+            foundBook->returnBook();
+        }else{
+            cout << "Searched book does not exist" << endl;
+        }
+    } else {
+        cout << "I don't understand." << endl;
     }
 
-     Book* foundBook = library.findBook("1984");
 
-    if(foundBook != nullptr){
-        cout << foundBook->getTitle() << " exists.";
-    }else{
-        cout << "Searched book does not exist";
-    }
+
+
+    
+
+    library.printLibrary();
+
+
+    //library.~Library(); // Generally, bad form
     
 
     return 0;
